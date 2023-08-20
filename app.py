a0c3d0267deb3f695a91582b35ddbb71b50218e3
Aditya Mondkar <adityamondkar111@gmail.com>
2023-08-20 15:32:01 +0530

    initial

---
+++ app.py
@@ -0,0 +1,38 @@
+import subprocess
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+def run_command(command):
+    try:
+        result = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, text=True)
+        return result
+    except subprocess.CalledProcessError as e:
+        return e.output
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        command = request.form['command']
+        result = run_command(command)
+        return render_template('index.html', result=result, command=command)
+    else:
+        return render_template('index.html')
+
+
+@app.route('/message',methods=['POST'])
+def message():
+    message = request.form.get('message')
+    messageContent = request.form.get('messageContent')
+
+    messageFinal = f'{message} {messageContent}'
+    run_command(messageFinal)
+    return redirect(url_for('index'))
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True,port=80)
+
+
+# ngrok tunnel --label edge=edghts_2UDFiv38g1FO1S8WDeizZ4RAhRK http://localhost:80
